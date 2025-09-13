47fc9687a59938805c83b3c805b1e471f4c5f027
fpradomatheus <fpradomatheus@gmail.com>
2025-09-13 01:11:40 -0300

    Adicionar função subtracao e teste

--- test_main.py
+++ test_main.py
@@ -5,6 +5,10 @@ class TestSoma(unittest.TestCase):
     def test_soma_basica(self):
         self.assertEqual(soma(2, 3), 5)
         self.assertEqual(soma(-1, 1), 0)
+    
+    def test_subtracao_basica(self):
+        self.assertEqual(subtracao(5, 3), 2)
 
 if __name__ == "__main__":
     unittest.main()
+
