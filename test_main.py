b6bd00852f9e9ad006cbb913b8dcbe4e136218c0
fpradomatheus <fpradomatheus@gmail.com>
2025-09-22 17:39:03 -0300

    Somativa2: adicionar testes unitários e garantir CI

--- test_main.py
+++ test_main.py
@@ -1,18 +1,29 @@
 import unittest
 from main import soma, subtracao
 
-class TestOperações(unittest.TestCase):
+class TestOperacoes(unittest.TestCase):
 
     def test_soma_basica(self):
         self.assertEqual(soma(2, 3), 5)
         self.assertEqual(soma(-1, 1), 0)
-    
+
+    def test_soma_floats(self):
+        self.assertAlmostEqual(soma(2.5, 3.1), 5.6, places=7)
+
+    def test_soma_negativos(self):
+        self.assertEqual(soma(-1, -1), -2)
+
     def test_subtracao_basica(self):
         self.assertEqual(subtracao(5, 3), 2)
+
+    def test_subtracao_negativos(self):
+        self.assertEqual(subtracao(-1, -1), 0)
+
+    def test_zero(self):
+        self.assertEqual(soma(0, 0), 0)
 
 if __name__ == "__main__":
     unittest.main()
 
-#teste correcao
-#teste correcao 2
+
 
